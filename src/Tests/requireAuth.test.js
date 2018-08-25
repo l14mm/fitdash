cd27c704a9507fa426be2653ca7faab009b88a31
liammoroney <liam.moroney14@gmail.com>
2018-08-25 21:12:46 +0100

    Added setup for jest tests.

--- src/Tests/requireAuth.test.js
+++ src/Tests/requireAuth.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
+
 import { Provider } from "react-redux";
+import RequireAuth from '../components/requireAuth';
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
@@ -11,8 +12,6 @@ const mockStore = configureMockStore(middlewares)
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={mockStore}>
-      <App />
-      </Provider>, div);
+    <RequireAuth/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
