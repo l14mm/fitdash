da849072a8aa12502bb8a2800af7c225c7f16151
liammoroney <liam.moroney14@gmail.com>
2018-10-17 21:00:49 +0100

    Removed wait for it script, decided to add support to node server to handle reconnecting to mongodb instead.

--- fitdash-api/app.js
+++ fitdash-api/app.js
@@ -14,76 +14,85 @@ var MongoStore = require('connect-mongo')(session);
 
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb/');
+mongoose.connect('mongodb://mongodb/',
+  {
+    server: {
+      auto_reconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
+    }
+  });
 var db = mongoose.connection;
 
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log("connected to mongodb!");
-});
 
-//use sessions for tracking logins
-app.use(session({
-  cookie: {
-    maxAge: 30 * 60 * 10000,
-    secure: false
-  },
-  secret: 'foo',
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
-  })
-}));
+  //use sessions for tracking logins
+  app.use(session({
+    cookie: {
+      maxAge: 30 * 60 * 10000,
+      secure: false
+    },
+    secret: 'foo',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('foo'));
-app.use(express.static(path.join(__dirname, 'public')));
+  // app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser('foo'));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// Add headers
-app.use(function (req, res, next) {
+  // Add headers
+  app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+    // Pass to next layer of middleware
+    next();
+  });
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+  app.use('/', indexRouter);
+  // app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
 });
 
 module.exports = app;
